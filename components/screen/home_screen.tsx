7f60bc54f0caaf0adcef91f4d7798a24ddd16f78
HandsomeChoco <kkb931115@gmail.com>
2022-09-08 01:25:23 +0900

    Designing Home Screen

--- components/screen/home_screen.tsx
+++ components/screen/home_screen.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import {ScrollView, Text, View, StyleSheet} from 'react-native';
+
+function Events() {
+  return (
+    <View style={style.event}>
+      <Text>Events</Text>
+    </View>
+  );
+}
+
+function Body() {
+  return (
+    <View>
+      <Text>Body</Text>
+    </View>
+  );
+}
 
 function Home() {
   return (
-    <ScrollView>
-      <Text>Home</Text>
+    <ScrollView style={style.home}>
+      <Events />
+      <Body />
     </ScrollView>
   );
 }
+
+const style = StyleSheet.create({
+  home: {
+    flexDirection: 'column',
+  },
+  event: {
+    backgroundColor: '#120f3b',
+  },
+});
 
 export default Home;
